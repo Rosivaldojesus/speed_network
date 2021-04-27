b7e3bcd649af4d9a2a310340b8dd42cbfa27ee52
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-04-27 18:55:29 -0300

    Inicio dash index

--- apps/stock/views.py
+++ apps/stock/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum
+from django.db.models import Sum, Q
 from django.shortcuts import render
 from .models import Produto
 from .models import EntradaProduto, SaidaProduto, TotalProdutos
@@ -6,6 +6,14 @@ from .models import EntradaProduto, SaidaProduto, TotalProdutos
 # Create your views here.
 def Index(request):
     produtos = Produto.objects.all()
+    queryset = request.GET.get('q')
+    if queryset:
+        produtos = Produto.objects.filter(
+            Q(nome_produto__icontains=queryset) |
+            Q(marca_produto__icontains=queryset)|
+            Q(modelo_produto__icontains=queryset)
+        )
+        
     return render(request, 'stock/index.html', {'produtos': produtos})
 
 def EntradaProdutos(request):
