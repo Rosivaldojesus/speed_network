7569546f56cc6ac88a2a8cce7ac1c91ab6645d5d
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-05-18 18:25:28 -0300

    Add campo data finalizacao

--- apps/core/views.py
+++ apps/core/views.py
@@ -33,9 +33,9 @@ def Index(request):
 
 
 
-    data = Instalacao.objects.filter(concluido='True').values('data_finalizacao').annotate(number=Count('id')).order_by('data_finalizacao')[:7]
+    data = Instalacao.objects.filter(concluido='True').values('data_finalizacao').annotate(number=Count('id')).order_by('data_finalizacao')[:10]
     #data =  Order.objects.filter().extra({'day':"Extract(day from created)"}).values_list('day').annotate(Count('id'))
-    dataServico = Servico.objects.filter(status_concluido='True').values('data_finalizacao').annotate(number=Count('id')).order_by('data_finalizacao')[:7]
+    dataServico = Servico.objects.filter(status_concluido='True').values('data_finalizacao').annotate(number=Count('id')).order_by('data_finalizacao')[:10]
 
 
 
