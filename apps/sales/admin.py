65402b0b56da4a04a5225b3cb12fb11d593dafd6
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-05-18 14:47:40 -0300

    Add campo data finalizacao

--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -4,8 +4,8 @@ from .models import Instalacao
 
 # Register your models here.
 class InstalacaoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome_cliente','status_agendada' ,'boleto_entregue','data_instalacao','concluido')
-    list_editable = ('nome_cliente','status_agendada' , 'boleto_entregue','data_instalacao','concluido')
+    list_display = ('id', 'nome_cliente','status_agendada' ,'boleto_entregue','data_instalacao','data_finalizacao','concluido')
+    list_editable = ('nome_cliente','status_agendada' , 'boleto_entregue','data_instalacao','data_finalizacao','concluido')
 
 
 admin.site.register(Instalacao, InstalacaoAdmin)
