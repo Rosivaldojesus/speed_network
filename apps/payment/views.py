7cc6916c41b886706833bf2622c8e9da9dc8765a
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-05-21 16:40:26 -0300

    Filtros de pagamentos

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -4,14 +4,17 @@ from .forms import CadastarPagamentoForm
 from django.contrib import messages
 from django.db.models import Count, Sum
 from django.db.models.functions import TruncMonth
+from django.db.models.functions import ExtractMonth
+
 # Create your views here.
 def Index(request):
 
-    dia = Pagamento.objects.filter().values('data_pagamento').annotate(number=Sum('valor_pagamento')).order_by('-data_pagamento')
+    #dia = Pagamento.objects.filter().values('data_pagamento').annotate(number=Sum('valor_pagamento')).order_by('-data_pagamento')
 
+    mes = Pagamento.objects.annotate(month=ExtractMonth('data_pagamento')).values('month').annotate(count=Sum('valor_pagamento')).values('month','count')
     pagamentos = Pagamento.objects.all()
     return render(request, 'payment/index.html', {'pagamentos':pagamentos,
-                                                  'dia': dia,
+                                                  'mes': mes,
                                                 })
 
 def CadastrarPagamento(request):
