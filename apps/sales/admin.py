d2e4a4de138cbdd2b92e86fc13202abccaabc0e9
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-06-11 16:50:43 -0300

    Data em boletos

--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -4,10 +4,9 @@ from .models import Instalacao
 
 # Register your models here.
 class InstalacaoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome_cliente','status_agendada' ,'boleto_entregue',
-                    'data_instalacao','data_finalizacao','concluido','boleto_entregue',
-                    'funcionario_instalacao',
-                    'instalacao_criado_por', 'instalacao_finalizado_por','instalacao_vendedor')
+    list_display = ('id', 'nome_cliente','status_agendada',
+                    'data_criacao',
+                    'instalacao_vendedor')
     list_filter = ('status_agendada','instalacao_vendedor')
 
 
