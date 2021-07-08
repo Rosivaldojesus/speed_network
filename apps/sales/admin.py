597dfaa9ad48adc7b904b608f3a394e95750727d
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-07-08 19:46:32 -0300

    Resumo de instalçaões

--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -5,7 +5,7 @@ from .models import Instalacao, ClientesVoip
 # Register your models here.
 class InstalacaoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome_cliente','sobrenome_cliente',
-                    'data_criacao',
+                    'data_finalizacao',
                     'instalacao_vendedor')
     list_filter = ('data_criacao','instalacao_vendedor')
     search_fields = ('nome_cliente', 'sobrenome_cliente')
