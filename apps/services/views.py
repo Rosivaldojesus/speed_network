135d477931a38eed0b89527d05218689fcbeb9b5
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-08-02 12:40:56 -0300

    Filtros dos serviços agendados

--- apps/services/views.py
+++ apps/services/views.py
@@ -85,9 +85,11 @@ def ServicosAgendados(request):
     if queryset:
         agendados = Servico.objects.filter(
             Q(contato_servico=queryset) |
-            Q(endereco_servico__icontains=queryset))
+            Q(endereco_servico__icontains=queryset)).filter(status_concluido='False')
+        quant_agendados = Servico.objects.filter(status_agendado='True').filter(status_concluido='False').count()
     if startdate:
-        agendados = Servico.objects.filter( Q(data_agendada__exact=startdate))
+        agendados = Servico.objects.filter( Q(data_agendada__exact=startdate)).filter(status_concluido='False')
+        quant_agendados = Servico.objects.filter(status_agendado='True').filter(status_concluido='False').count()
 
     quant_agendados = Servico.objects.filter(status_agendado='True').filter(status_concluido='False').count()
     return render(request, 'services/servicos-agendados.html', {'agendados': agendados,
