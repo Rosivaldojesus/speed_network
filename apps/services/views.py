4c2308364d71042a9e6268bfab231a0262a0da2e
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-10-26 09:59:34 -0300

    Gráfico de servicos mensal

--- apps/services/views.py
+++ apps/services/views.py
@@ -21,7 +21,7 @@ def Index(request):
    # diarioInstalaçao = Instalacao.objects.filter(concluido='True').values('data_finalizacao').annotate( number=Count('data_finalizacao')).order_by('data_finalizacao')[90:]
     servicosMensal = Servico.objects.annotate(month=TruncMonth('data_finalizacao')).filter(status_concluido='True').values('month').annotate(c=Count('data_finalizacao')).values('month', 'c').order_by('month')
 
-    diarioServicos = Servico.objects.filter(status_concluido='True').values('data_finalizacao').annotate(number=Count('data_finalizacao')).order_by('data_finalizacao')
+    diarioServicos = Servico.objects.filter(status_concluido='True').values('data_finalizacao').annotate(number=Count('data_finalizacao')).order_by('data_finalizacao')[90:]
 
 
 
