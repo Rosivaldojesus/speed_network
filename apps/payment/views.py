449b393cdb92ff41659ce86d8ee4fb9d7aa2d5e7
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-11-03 16:53:35 -0300

    Falha na lista de pagamentos

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -24,7 +24,7 @@ def Index(request):
 
     #Query para o total de gastos de cada mês
     mes =  Pagamento.objects.annotate(month=TruncMonth('data_pagamento')).filter(data_pagamento__lt=data_atual).filter().values('month').annotate(c=Sum('valor_pagamento')).values('month', 'c').order_by('month')
-    custo_mes =  Pagamento.objects.annotate(month=ExtractMonth('data_pagamento')).filter(data_pagamento__lt=data_atual).filter().values('month').annotate(c=Sum('valor_pagamento')).values('month', 'c').order_by('month')
+    custo_mes =  Pagamento.objects.annotate(month=TruncMonth('data_pagamento')).filter().values('month').annotate(c=Sum('valor_pagamento')).values('month', 'c').order_by('month')
 
     pagamentos = Pagamento.objects.all().order_by('-data_pagamento')
 
