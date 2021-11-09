d24468618fb2e52f861ec39d8b44cc1a2f046f91
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-11-09 20:27:24 -0300

    Add controle de boletos

--- apps/payment/admin.py
+++ apps/payment/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from .models import CategoriaPagamento, Pagamento, TipoCusto, OrigemValores
 from .models import AgendaPagamento, FluxoEntradasSaidas, DestinoValoresBoletos
 
+from .models import FormasEntregaBoletos, EntregaBoletos
+
 # Register your models here.
 
 admin.site.register(CategoriaPagamento)
 admin.site.register(TipoCusto)
 admin.site.register(OrigemValores)
+admin.site.register(FormasEntregaBoletos)
+admin.site.register(EntregaBoletos)
 
 
 class AgendaPagamentoForm(admin.ModelAdmin):
