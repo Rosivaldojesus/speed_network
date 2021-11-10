bc8b0a69322a25cfc15950f54567d12dba4ac622
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-11-10 13:37:15 -0300

    add código cto

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -452,14 +452,8 @@ class EntregaBoletosListView(ListView):
 
         queryset = self.request.GET.get('q')
         if queryset:
-            context['lista_boletos_entregue'] = ClientesEntregaBoletos.objects.filter(
-                Q(nome_cliente__icontains=queryset),
-                Q(cpf_cliente__icontains=queryset)
-            )
-            context['count_boletos_entregue'] = ClientesEntregaBoletos.objects.filter(
-                Q(nome_cliente__icontains=queryset),
-                Q(cpf_cliente__icontains=queryset)
-            ).count()
+            context['lista_boletos_entregue'] = ClientesEntregaBoletos.objects.filter(Q(nome_cliente__icontains=queryset),Q(cpf_cliente__icontains=queryset))
+            context['count_boletos_entregue'] = ClientesEntregaBoletos.objects.filter(Q(nome_cliente__icontains=queryset), Q(cpf_cliente__icontains=queryset)).count()
 
         else:
             context['lista_boletos_entregue'] = ClientesEntregaBoletos.objects.all()
