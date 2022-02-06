b9e222f0c9da68e89e61ebe4de6e5ef342a0856f
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-02-06 19:16:24 -0300

    Painel Financeiro

--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -3,24 +3,32 @@ from .models import Instalacao, ClientesVoip, ValeRefeicao, Cancelamentos
 
 
 # Register your models here.
+
+
 class InstalacaoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome_cliente','planos_instalacao','sobrenome_cliente',
-                    'data_finalizacao',
+    list_display = ('id', 'nome_cliente', 'planos_instalacao', 'sobrenome_cliente', 'data_finalizacao',
                     'instalacao_vendedor', 'como_conheceu_empresa')
-    list_filter = ('data_criacao','instalacao_vendedor')
+    list_filter = ('data_criacao', 'instalacao_vendedor')
     search_fields = ('nome_cliente', 'sobrenome_cliente')
+
+
 admin.site.register(Instalacao, InstalacaoAdmin)
+
 
 admin.site.register(ClientesVoip)
 
 
-
 class ValeRefeicaoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome_funcionario','data_criacao','data_vale','valor_vale','status_pago')
+    list_display = ('id', 'nome_funcionario', 'data_criacao', 'data_vale', 'valor_vale', 'status_pago')
+
+
 admin.site.register(ValeRefeicao, ValeRefeicaoAdmin)
 
 
-
 class CancelamentosAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'plano_internet','data_criacao','atendente')
+    list_display = ('id', 'nome', 'plano_internet', 'data_criacao', 'atendente')
+    list_editable = ('nome', 'plano_internet', 'atendente')
+    search_fields = ('nome',)
+
+
 admin.site.register(Cancelamentos, CancelamentosAdmin)
--- apps/sales/models.py
+++ apps/sales/models.py
@@ -1,11 +1,7 @@
-from datetime import datetime, date
-
 from django.utils import timezone
 from ..components.models import PlanosInternet, Cidade, DataVencimento, Vendedores
 from django.db import models
 from django.contrib.auth.models import User
-from ..components.models import FuncionariosParaVale
-from ckeditor.fields import RichTextField
 from django.urls import reverse
 
 
@@ -16,7 +12,8 @@ class Instalacao(models.Model):
     sobrenome_cliente = models.CharField(max_length=255, blank=True, null=True, verbose_name='Sobrenome')
     cpf_cliente = models.CharField(max_length=20, blank=True, null=True, verbose_name='CPF')
     rg_cliente = models.CharField(max_length=20, blank=True, null=True, verbose_name='RG')
-    data_nascimento_cliente = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True, verbose_name='Data de Nascimento')
+    data_nascimento_cliente = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True,
+                                               verbose_name='Data de Nascimento')
     cep_cliente = models.CharField(max_length=20, blank=True, null=True, verbose_name='CEP')
     rua_cliente = models.CharField(max_length=100, blank=True, null=True, verbose_name='Rua')
     bairro_cliente = models.CharField(max_length=100, blank=True, null=True, verbose_name='Bairro')
@@ -31,16 +28,21 @@ class Instalacao(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     status_agendada = models.BooleanField(default=False)
     concluido = models.BooleanField(default=False)
-    data_instalacao = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True, verbose_name='Data da Instalação')
-    hora_instalacao = models.TimeField(auto_now_add=False, auto_now=False, blank=True, null=True, verbose_name='Hora da Instalação')
+    data_instalacao = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True,
+                                       verbose_name='Data da Instalação')
+    hora_instalacao = models.TimeField(auto_now_add=False, auto_now=False, blank=True, null=True,
+                                       verbose_name='Hora da Instalação')
     material_utilizado = models.TextField(blank=True, null=True, verbose_name='Material utilizado')
     funcionario_instalacao = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
     data_concluido = models.DateTimeField(null=True, blank=True)
-    data_finalizacao = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True, verbose_name='Data da Finalização')
+    data_finalizacao = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True,
+                                        verbose_name='Data da Finalização')
     boleto_entregue = models.BooleanField(default=False)
     observacao_instalacao = models.TextField(blank=True, null=True, verbose_name='Observação')
-    instalacao_criado_por = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='instalacao_criado_por', blank=True, null=True)
-    instalacao_finalizado_por = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='instalacao_finalizado_por', blank=True, null=True)
+    instalacao_criado_por = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='instalacao_criado_por',
+                                              blank=True, null=True)
+    instalacao_finalizado_por = models.ForeignKey(User, on_delete=models.DO_NOTHING,
+                                                  related_name='instalacao_finalizado_por', blank=True, null=True)
     instalacao_vendedor = models.ForeignKey(Vendedores, on_delete=models.DO_NOTHING, verbose_name='Vendedores')
     como_conheceu_empresa = models.CharField(max_length=255, blank=True, null=True)
 
@@ -63,7 +65,8 @@ class ClientesVoip(models.Model):
     data_reserva_voip = models.DateField(blank=True, null=True, verbose_name='Data da Reserva')
     portabilidade_voip = models.BooleanField(default=False, verbose_name='Portabilidade')
     finalizado_voip = models.BooleanField(default=False, verbose_name='Finalizado')
-    funcionario_reserva_voip = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='funcionario_reserva_voip', blank=True, null=True)
+    funcionario_reserva_voip = models.ForeignKey(User, on_delete=models.DO_NOTHING,
+                                                 related_name='funcionario_reserva_voip', blank=True, null=True)
 
     objects = models.Manager()
 
@@ -72,7 +75,6 @@ class ClientesVoip(models.Model):
 
     def __str__(self):
         return "{}".format(self.nome_usuario_voip)
-
 
 
 class ValeRefeicao(models.Model):
@@ -91,8 +93,6 @@ class ValeRefeicao(models.Model):
         return "{}".format(self.nome_funcionario)
 
 
-
-
 class Cancelamentos(models.Model):
     nome = models.CharField(max_length=255, blank=True, null=True, verbose_name='Nome Cliente')
     cpf = models.CharField(max_length=30, verbose_name='CPF do Cliente')
@@ -100,8 +100,6 @@ class Cancelamentos(models.Model):
     cep = models.CharField(max_length=255, verbose_name='CEP do Cliente')
     bairro = models.CharField(max_length=255, verbose_name='Bairro do Cliente')
     numero = models.CharField(max_length=255, verbose_name='Número do Cliente')
-
-
     atendente = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True, verbose_name='Atendente')
     plano_internet = models.CharField(max_length=255, blank=True, null=True)
     motivo = models.CharField(max_length=255, blank=True, null=True)
