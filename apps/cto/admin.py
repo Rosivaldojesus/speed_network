ba821e90c8662c0b042a3f614b86587708b5d78b
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-02-06 20:02:32 -0300

    Painel Financeiro

--- apps/cto/admin.py
+++ apps/cto/admin.py
@@ -4,24 +4,39 @@ from .models import TerminaisOpticos, CaixasDeEmenda, PonPorCaixaEmenda, NumeroP
 
 # Register your models here.
 
+
 class TerminaisOpticosAdmin(admin.ModelAdmin):
-    list_display = ('id', 'rua_cto','numero_rua_cto','codigo_cto','board_cto', 'pon_cto','conexoes_opticas_cto','quant_conexoes_usadas_cto')
+    list_display = (
+        'id', 'bairro_cto', 'rua_cto', 'numero_rua_cto', 'codigo_cto', 'board_cto', 'pon_cto',
+        'conexoes_opticas_cto', 'quant_conexoes_usadas_cto'
+    )
+
+
 admin.site.register(TerminaisOpticos, TerminaisOpticosAdmin)
 
+
 class CaxasDeEmendaAdmin(admin.ModelAdmin):
-    list_filter = ['codigo_caixa','rua_caixa_emenda','numero_rua_cto']
+    list_filter = ['codigo_caixa', 'rua_caixa_emenda', 'numero_rua_cto']
+
+
 admin.site.register(CaixasDeEmenda, CaxasDeEmendaAdmin)
+
 
 admin.site.register(PonPorCaixaEmenda)
 
+
 admin.site.register(NumeroPon)
+
 
 class PrimariasAdmin(admin.ModelAdmin):
     list_display = ('board', 'pon', 'localizacao', 'quant_caixas')
-admin.site.register(Primarias, PrimariasAdmin )
+
+
+admin.site.register(Primarias, PrimariasAdmin)
 
 
 class CaixasDasPrimariasAdmin(admin.ModelAdmin):
     list_display = ('primaria', 'logradouro', 'logradouro_numero')
-admin.site.register(CaixasDasPrimarias, CaixasDasPrimariasAdmin)
 
+
+admin.site.register(CaixasDasPrimarias, CaixasDasPrimariasAdmin)
