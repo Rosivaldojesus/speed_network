43658a3fb6fe59dfd22665455e015b54b6a32005
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-03-29 22:23:07 -0300

    Filtro de ruas

--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -6,10 +6,10 @@ from .models import Instalacao, ClientesVoip, ValeRefeicao, Cancelamentos
 
 
 class InstalacaoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome_cliente', 'planos_instalacao', 'sobrenome_cliente', 'data_finalizacao',
+    list_display = ('id', 'nome_cliente','sobrenome_cliente', 'planos_instalacao', 'rua_cliente' , 'data_finalizacao',
                     'instalacao_vendedor', 'como_conheceu_empresa')
     list_filter = ('data_criacao', 'instalacao_vendedor')
-    search_fields = ('nome_cliente', 'sobrenome_cliente')
+    search_fields = ('nome_cliente', 'sobrenome_cliente', 'rua_cliente')
 
 
 admin.site.register(Instalacao, InstalacaoAdmin)
