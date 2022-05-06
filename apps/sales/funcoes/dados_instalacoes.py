d991b52aaf9067cb39b6783380701e9f2772f2c1
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-05-06 15:16:21 -0300

    Correção query mês atual

--- apps/sales/funcoes/dados_instalacoes.py
+++ apps/sales/funcoes/dados_instalacoes.py
@@ -13,5 +13,5 @@ last_month = (datetime.today() - timedelta(days=30)).month
 
 
 def instalacoes_mes_atual():
-    total = Instalacao.objects.filter(data_finalizacao__month=this_month).count()
+    total = Instalacao.objects.filter(data_finalizacao__month=5, concluido='True').count()
     return total
